0d77b7f6b41a8cdedb79abbfe0b1e6777de58d31
Alan Santos <alanssantos32@gmail.com>
2019-03-15 22:16:40 -0300

    Prontos!

--- Exercícios/classePonto.py
+++ Exercícios/classePonto.py
@@ -1,3 +1,4 @@
+from math import sqrt
 class Ponto:
     def __init__(self, x, y):
         self.x =  x
@@ -12,3 +13,16 @@ class Ponto:
         return self.x
     def getY(self):
         return self.y
+
+    def distanciaOrigem(self):
+        distanciaX = (0 - self.x) * (0 - self.x)
+        distanciaY = (0 - self.y) * (0 - self.y)
+        return  sqrt(distanciaX + distanciaY)
+    def distanciaEntrePontos(self, ponto):
+        distanciaX = (ponto.getX() - self.x) * (ponto.getX() - self.x)
+        distanciaY = (ponto.getY() - self.y) * (ponto.getY() - self.y)
+        return  sqrt(distanciaX + distanciaY)
+
+ponto1 = Ponto(int(input('Coordenada x: ')), int(input('Coordenada y: ')))
+ponto2 = Ponto(int(input('Coordenada x: ')), int(input('Coordenada y: ')))
+print('Distância da oridem: {}\nDistância entre pontos: {}'.format(ponto1.distanciaOrigem(), ponto1.distanciaEntrePontos(ponto2)))
--- Exercícios/classeTriangulo.py
+++ Exercícios/classeTriangulo.py
@@ -7,10 +7,22 @@ class Triangulo:
     def equilatero(self):
         return True if self.p1 == self.p2 and self.p1 == self.p3 else False
     def isosceles(self):
-        teste1 = True if (self.p1 == self.p2 and self.p1) != self.p3
-        teste2 = True if (self.p1 == self.p2 and self.p1 != self.p3
-        teste3 = True if (self.p1 == self.p2 and self.p1) != self.p3
-        return True if self.p1 == self.p2 and self.p1 != self.p3
+        teste1 = True if self.p1 == self.p2 and self.p1 != self.p3 else False
+        teste2 = True if self.p1 == self.p3 and self.p1 != self.p2 else False
+        teste3 = True if self.p2 == self.p3 and self.p2 != self.p1 else False
+        return teste1 or teste2 or teste3
+    def escaleno(self):
+        teste1 = True if self.p1 != self.p2 else False
+        teste2 = True if self.p1 != self.p3 else False
+        teste3 = True if self.p3 != self.p2 else False
+        return teste1 and teste2 and teste3
 
-obj = Triangulo(1, 1, 1)
-print(obj.equilatero())
+    def perimetro(self):
+        return self.p1 + self.p2 + self.p3
+
+obj1 = Triangulo(1, 1, 1)
+obj2 = Triangulo(2, 1, 1)
+obj3 = Triangulo(2, 1, 3)
+print(obj1.equilatero(), obj2.isosceles(), obj3.escaleno(), obj1.perimetro())
+print(obj2.equilatero(), obj3.isosceles(), obj1.escaleno(), obj2.perimetro())
+print(obj3.equilatero(), obj1.isosceles(), obj2.escaleno(), obj3.perimetro())
