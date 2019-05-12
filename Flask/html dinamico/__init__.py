9b72ca83de595129a5c038a78e3feda6c00aa871
Alan Santos <alanssantos32@gmail.com>
2019-05-12 15:54:23 -0300

    Metade do exercício

---
+++ Flask/html dinamico/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__, template_folder='template')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/usuario', methods = ['GET', 'POST'])
+def register():
+    if request.method !='POST':
+       return render_template('form.html')
+
+    else:
+        username = request.form['name']
+        return render_template('view.html', name=username)
+
+@app.route('/busca/<int:cod>')
+def search(cod):
+    return render_template('search.html', name=cod)
+
+def main():
+    app.env = 'development'
+    app.run(debug=True, port=7000)
+
+if __name__ == "__main__":
+    main()
