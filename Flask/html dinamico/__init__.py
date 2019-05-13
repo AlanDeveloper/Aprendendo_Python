1435aa5bef43d3bacd228ef41697607aabccdb30
Alan Santos <alanssantos32@gmail.com>
2019-05-12 23:37:16 -0300

    up up

--- Flask/html dinamico/__init__.py
+++ Flask/html dinamico/__init__.py
@@ -9,12 +9,10 @@ def index():
 
 @app.route('/usuario', methods = ['GET', 'POST'])
 def register():
-    if request.method !='POST':
-       return render_template('form.html')
-
-    else:
+    if request.method =='POST':
         username = request.form['name']
         return render_template('view.html', name=username)
+    return render_template('form.html')
 
 @app.route('/busca/<int:cod>')
 def search(cod):
