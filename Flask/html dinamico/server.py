b22eb63f5ba6b57257ef32a06fbe877d9dd5ba8d
Alan Santos <alanssantos32@gmail.com>
2019-05-14 20:21:36 -0300

    atoa

--- Flask/html dinamico/server.py
+++
@@ -1,7 +0,0 @@
-#!-*- conding: utf8 -*-
-import psycopg2
-
-class serverDAO:
-    def conectar(self):
-        banco = "dbname=flask user=postgres password=postgres host=localhost port=5432"
-        return psycopg2.connect(banco)
