dbe42e4093d20f7e8b542779f0d291e6fbf9c299
Alan Santos <alanssantos32@gmail.com>
2019-05-28 09:16:03 -0300

    Add files via upload

--- Flask/sessão/main.py
+++ Flask/sessão/main.py
@@ -1,30 +1,56 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from funcDAO import funcDAO
 
 app = Flask(__name__, template_folder='template')
 
+@app.before_first_request
+def  before():
+    session['logged_in'] = False
+
+@app.route('/')
+def redi():
+    return redirect('/func')
+
 @app.route('/func', methods=['GET', 'POST'])
 def index():
-    if request.method =='POST':
-        res = funcDAO().login(request.form['login'], request.form['pass'])
+    if request.method == 'POST':
+        if session['logged_in'] == False:
+            res = funcDAO().login(request.form['login'], request.form['pass'])
 
-        if res == True:
-            session['username'] = request.form['login']
-            session['password'] = request.form['pass']
-            return render_template('logado.html', date=session)
-        else:
-            return 'Dados incorretos'
+            if res:
+                session['logged_in'] = True
+                session['username'] = request.form['login'] or session['username']
+                session['password'] = request.form['pass'] or session['password']
+                return redirect('/func/view')
+            else:
+                return 'Dados incorretos'
+        else: 
+            return redirect('/func/view')
     return render_template('form.html')
 
-@app.route('/exit')
+@app.route('/func/view')
+def dados():
+    if session['logged_in']:
+        return render_template('logado.html', date=session)
+    return redirect('/func')
+
+@app.route('/func/exit')
 def exit():
-    session = []
+    session['logged_in'] = False
     return 'deslogado <a href="/func">Home</a>'
+
+@app.route('/func/add', methods=['GET', 'POST'])
+def add():
+    if session['logged_in']:
+        if request.method =='POST':
+            return 'registrado'
+        return render_template('add.html')
+    return redirect('/func')
 
 def main():
     app.secret_key = 'minha chave'
     app.env = 'development'
-    app.run(debug=True, port=7000)
+    app.run(debug=True, port=2001)
 
 if __name__ == "__main__":
     main()
